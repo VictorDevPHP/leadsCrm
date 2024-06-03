08da49ffb270c21a29b6480a00aa6263e7055310
Victor Vinicius <victorsomulex@gmail.com>
2024-06-03 15:02:21 -0300

    feat: Add WppApi methods for sending messages, listing sessions, and connecting to WhatsApp server

--- app/Http/Controllers/API/wpp/WppApi.php
+++ app/Http/Controllers/API/wpp/WppApi.php
@@ -15,7 +15,8 @@ class WppApi extends Controller
      * @param string $key The API key for authentication.
      * @return array An array containing the status code and response from the API.
      */
-    public function sendMessage(string $number, string $message, string $key): array{
+    public function sendMessage(string $number, string $message, string $key): array
+    {
         $url = 'http://localhost:3000/wppconnect/sendMessage';
         $data = array('phone' => $number, 'text' => $message, 'key' => $key);
         $ch = curl_init($url);
@@ -23,6 +24,48 @@ class WppApi extends Controller
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        $response = curl_exec($ch);
+        $statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+        return array('status_code' => $statusCode, 'response' => json_decode($response, true));
+    }
+
+    /**
+     * Retrieves a list of sessions from the WppConnect API.
+     *
+     * @return array An array containing the status code and the response data.
+     */
+    public function listSessions(): array
+    {
+        $url = 'http://localhost:3000/wppconnect/listSessions';
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        $response = curl_exec($ch);
+        $statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+        return array('status_code' => $statusCode, 'response' => json_decode($response, true));
+    }
+
+    /**
+     * Connects to the WhatsApp server.
+     *
+     * @param string $sessionName The name of the session.
+     * @return array An array containing the status code and response from the API.
+     */
+    public function connect(string $sessionName): array
+    {
+        $url = 'http://localhost:3000/wppconnect/connect';
+        $data = array('sessionName' => $sessionName);
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+    
+        // Limit the total time that cURL can execute to 2 seconds
+        curl_setopt($ch, CURLOPT_TIMEOUT, 2);
+    
         $response = curl_exec($ch);
         $statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         curl_close($ch);
