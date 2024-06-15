5368609570223928e931017d6d3e2ae6ccb56717
Victor Vinicius <victorsomulex@gmail.com>
2024-06-15 20:14:08 -0300

    feat: Add Livewire component for managing invited users

---
+++ app/Livewire/Users/InvitedUsers.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Users;
+
+use Livewire\Component;
+
+class InvitedUsers extends Component
+{
+    public function render()
+    {
+        return view('livewire.users.invited-users');
+    }
+}
---
+++ resources/views/livewire/users/invited-users.blade.php
@@ -0,0 +1,5 @@
+<x-slot>
+    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        {{ __('Usuários') }}
+    </h2>
+</x-slot>
