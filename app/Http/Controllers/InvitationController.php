0bca6331808cfcbf0c818fdf73f2a3b8ea856045
Victor Vinicius <victorsomulex@gmail.com>
2024-06-16 13:05:46 -0300

    feat: Refactor InvitationController to send invitations via email

--- app/Http/Controllers/InvitationController.php
+++ app/Http/Controllers/InvitationController.php
@@ -3,29 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Models\Invitation;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\InvitationMail;
 use Illuminate\Support\Str;
 
 class InvitationController extends Controller
 {
-    public function sendInvitation(Request $request)
+    /**
+     * Send an invitation to a user.
+     *
+     * @param array $data The data containing the email, profile, customer_id, etc.
+     * @return \Illuminate\Http\RedirectResponse The response redirecting back with a success message.
+     */
+    public function sendInvitation($data)
     {
-        $request->validate([
-            'email' => 'required|email|unique:invitations,email',
-        ]);
-
         $token = Str::random(32);
-
         $invitation = Invitation::create([
-            'email' => $request->email,
+            'email' => $data['email'],
             'token' => $token,
+            'profile' => $data['profile'],
+            'customer_id' => $data['customer_id'],
             'expires_at' => now()->addDays(7),
         ]);
-
-        Mail::to($request->email)->send(new InvitationMail($invitation));
-
+        Mail::to($data['email'])->send(new InvitationMail($invitation));
         return back()->with('success', 'Convite enviado com sucesso!');
     }
 }
