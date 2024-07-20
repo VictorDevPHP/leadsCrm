c2a069f9fab98b7a7e93229ef9240bfd193abebe
Victor Vinicius <victorsomulex@gmail.com>
2024-07-19 23:05:31 -0300

    fix: remve dd

--- app/Http/Controllers/API/OpenAI/Functions/ToSchedeule.php
+++ app/Http/Controllers/API/OpenAI/Functions/ToSchedeule.php
@@ -8,7 +8,6 @@ class ToSchedeule
     public static function toSchedeule($arguments)
     {
         $argumentsArray = (array) json_decode(json_encode($arguments), true);
-        dd(json_decode($argumentsArray[0], true));
         Log::info("Criando agendamento: ", $argumentsArray);
         return true;
     }
